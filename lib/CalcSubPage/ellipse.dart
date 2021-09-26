7a58444be8c3f3aff9dc314c9bcac110b9b0f6ca
pleumwongvarit <pleum.wongvarit@gmail.com>
2021-09-26 15:27:49 +0700

    add ellipse calcpage

---
+++ lib/CalcSubPage/ellipse.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class EllipseCalcPage extends StatefulWidget {
+  EllipseCalcPage({Key? key}) : super(key: key);
+
+  @override
+  _EllipseCalcPage createState() => _EllipseCalcPage();
+}
+
+class _EllipseCalcPage extends State<EllipseCalcPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Column(
+          children: [
+            TextField(
+              decoration: new InputDecoration(labelText: "a"),
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly
+              ],
+            ),
+            TextField(
+              decoration: new InputDecoration(labelText: "b"),
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly
+              ],
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
