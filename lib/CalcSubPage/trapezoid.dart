c8c6fbe1ac1ea2b53c2c8564cb99b2366b281d96
pleumwongvarit <pleum.wongvarit@gmail.com>
2021-10-19 15:53:05 +0700

    finish trapezoid

--- lib/CalcSubPage/trapezoid.dart
+++ lib/CalcSubPage/trapezoid.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:geomath/numberField.dart';
 
 class TrapezoidCalcPage extends StatefulWidget {
   TrapezoidCalcPage({Key? key}) : super(key: key);
@@ -9,6 +9,10 @@ class TrapezoidCalcPage extends StatefulWidget {
 }
 
 class _TrapezoidCalcPage extends State<TrapezoidCalcPage> {
+  num a = 0;
+  num b = 0;
+  num h = 0;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -16,18 +20,35 @@ class _TrapezoidCalcPage extends State<TrapezoidCalcPage> {
       child: Column(
         children: [
           Column(children: [
-            TextField(
-                decoration: new InputDecoration(labelText: "a (side)"),
-                keyboardType: TextInputType.number,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.digitsOnly
-                ]),
-            TextField(
-                decoration: new InputDecoration(labelText: "b (diagonal)"),
-                keyboardType: TextInputType.number,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.digitsOnly
-                ]),
+            NumberField(
+                labelText: "a",
+                onChanged: (value) => {
+                      setState(() {
+                        a = double.parse(value);
+                      })
+                    }),
+            NumberField(
+                labelText: "b",
+                onChanged: (value) => {
+                      setState(() {
+                        b = double.parse(value);
+                      })
+                    }),
+            NumberField(
+                labelText: "h (Height)",
+                onChanged: (value) => {
+                      setState(() {
+                        h = double.parse(value);
+                      })
+                    }),
+            Text(
+              "พื้นที่",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            ),
+            Text(
+              "${0.5 * h * (a + b)}",
+              style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+            ),
           ])
         ],
       ),
